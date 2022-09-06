a1367d47ff839ab738fcd47bfc935f68bb3d5214
Haary Richard <53508354+haaryrichardt@users.noreply.github.com>
2022-09-06 16:32:16 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,21 +4,9 @@ terraform {
       source  = "hashicorp/google"
       version = "~>3.88"
     }
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = "~>1.13"
-    }
     helm = {
       source  = "hashicorp/helm"
       version = "~>2.3"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "2.6.1"
-    }
-    time = {
-      source  = "hashicorp/time"
-      version = "0.7.2"
     }
   }
 }
@@ -50,40 +38,17 @@ provider "helm" {
 }
 
 
-###################### GKE Ingress ######################
+###################### Managed Ingress/Load Balancer ######################
 
-# Static IPv4 address for Ingress Load Balancing
-resource "google_compute_global_address" "ingress-ipv4" {
-  name       = "${var.cluster_name}-ingress-ipv4"
-  ip_version = "IPV4"
-}
+
   
-# Forward IPv4 TCP traffic to the HTTP proxy load balancer
-resource "google_compute_global_forwarding_rule" "ingress-http-ipv4" {
-  name        = "${var.cluster_name}-ingress-http-ipv4"
-  ip_address  = google_compute_global_address.ingress-ipv4.address
-  ip_protocol = "TCP"
-  port_range  = "80"
-  target      = google_compute_target_http_proxy.ingress-http.self_link
-}
+  
+ # TO-DO
+  
+  
 
-###################### Cloud Router ######################
+####################### Managed Ingress/Load Balancer ########################
 
-resource "google_compute_router" "router" {
-  name    = "test-devops-router"
-  region  = local.region
-  network = "default"
-}
-
-####################### Cloud NAT ########################
-
-resource "google_compute_router_nat" "nat" {
-  name                               = "test-devops-router-nat"
-  router                             = google_compute_router.router.name
-  region                             = google_compute_router.router.region
-  nat_ip_allocate_option             = "AUTO_ONLY"
-  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
-}
 
 module "gke_cluster" {
   source       = "./gke-cluster"
