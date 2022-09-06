8e4073f9aaf4ea0def266159be8aee4643995d7b
Haary Richard <53508354+haaryrichardt@users.noreply.github.com>
2022-09-06 11:52:53 +0100

    Update main.tf

--- helm/main.tf
+++ helm/main.tf
@@ -7,27 +7,6 @@ terraform {
   }
 }
 
-// Cert Manager Helm Chart
-resource "helm_release" "cert_manager" {
-  provider = helm
-  name       = "cert-manager"
-  repository = "https://charts.jetstack.io"
-  chart      = "cert-manager"
-  version    = "v1.5.4"
-
-  namespace        = "cert-manager"
-  create_namespace = true
-
-  set {
-    name  = "installCRDs"
-    value = "true"
-  }
-
-  set{
-    name = "extraArgs"
-    value = "{--dns01-recursive-nameservers-only}"
-  }
-}
 
 # Use either NGINX or Kong ingress
 
@@ -42,20 +21,3 @@ resource "helm_release" "cert_manager" {
 #   namespace        = "nginx-ingress"
 #   create_namespace = true
 # }
-
-# Kong Ingress Controller Helm Chart
-# Kong ingress controller proides API gateway solution to route all traffic into back-end services
-resource "helm_release" "kong_ingress_controller" {
-  provider = helm
-  name       = "kong"
-  repository = "https://charts.konghq.com"
-  chart      = "kong"
-  version    = "2.4.0"
-
-  namespace        = "kong"
-  create_namespace = true
-
-  depends_on = [
-    helm_release.cert_manager
-  ]
-}
