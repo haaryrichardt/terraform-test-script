e6892930dad74b324ebba23ae1e9d1d89ccd4e23
Haary Richard <53508354+haaryrichardt@users.noreply.github.com>
2022-09-06 14:06:24 +0100

    Update main.tf

--- helm/main.tf
+++ helm/main.tf
@@ -21,3 +21,17 @@ terraform {
 #   namespace        = "nginx-ingress"
 #   create_namespace = true
 # }
+
+# Use either NGINX or Kong ingress
+
+ // Nginx Ingress Controller Helm Chart
+ resource "helm_release" "nginx_ingress_controller" {
+   provider = helm
+   name       = "ingress-nginx"
+   repository = "https://kubernetes.github.io/ingress-nginx"
+   chart      = "ingress-nginx"
+   version    = "3.34.0"
+
+   namespace        = "nginx-ingress"
+   create_namespace = true
+ }
